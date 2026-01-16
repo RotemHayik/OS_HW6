4fbc983f226bfdf3f8fc93f880e644dbcd1cc5fe
RotemHayik <rotemhayik@mail.tau.ac.il>
2026-01-16 20:40:41 +0200

    client side is ready

--- pcc_client.c
+++ pcc_client.c
@@ -9,33 +9,39 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
 
+
+#define MAX_BUFFER_SIZE 1024
 
 
 int parse_args(int argc, char *argv[], struct sockaddr_in *server_addr, char **file_path) {
     
     // check the number of arguments
     if (argc != 4) {
-        perror("This program accepts exactly 3 arguments: server's IP address, server's port number, and a file path\n");
+        fprintf(stderr, "This program accepts exactly 3 arguments: server's IP address, server's port number, and a file path\n");
         return -1;
     }
+    memset(server_addr, 0, sizeof(*server_addr));
 
     // process each argument
+    // all arguments are char*
     const char *server_ip;
     server_ip = argv[1];
     // converting a string containing an IP address to binary representation
     // assumption: valid IP address is provided
-    inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
+    inet_pton(AF_INET, server_ip, &server_addr->sin_addr);
 
     // unsigned 16-bit integer for port number (given in host byte order)
     // assumption: valid port number is provided
     uint16_t server_port;
     server_port = (uint16_t)atoi(argv[2]);
     // converting port number to network byte order
-    server_addr.sin_port = htons(server_port);
+    server_addr->sin_port = htons(server_port);
 
-    const char *file_path;
-    file_path = argv[3];
+    server_addr->sin_family = AF_INET;
+
+    *file_path = argv[3];
 
     return 0;
 }
@@ -57,6 +63,7 @@ int TCP_connect(struct sockaddr_in *server_addr) {
         perror("Error creating socket\n");
         return -1;
     }
+
     if (connect(sockfd, (struct sockaddr *)server_addr, sizeof(*server_addr)) < 0) {
         perror("Error connecting to server\n");
         close(sockfd);
@@ -66,11 +73,67 @@ int TCP_connect(struct sockaddr_in *server_addr) {
 }
 
 
-void main(int argc, char *argv[]) {
+int transmit_data(int sockfd, int fd)
+{
+    char buffer[MAX_BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0) {
+        // starting a new buffer transmission
+        ssize_t total_written = 0;
+
+        // ensure all bytes from the current buffer are written
+        while (total_written < bytes_read) {
+            // write to socket
+            // start from the position after the last written byte
+            // the number of remaining bytes
+            ssize_t bytes_written = write(sockfd, buffer + total_written, bytes_read - total_written);
+            if (bytes_written < 0) {
+                perror("Error writing to socket\n");
+                return -1;
+            }
+            total_written += bytes_written;
+        }
+    }
+
+    if (bytes_read < 0){
+        perror("Error reading from file\n");
+        return -1;}
+
+    // if reached EOF, bytes_read == 0 and transmission is complete
+    return 0;
+}
+
+int receive_data(int sockfd, unsigned int *printable_count) {
+    char buffer[MAX_BUFFER_SIZE];
+    ssize_t bytes_received;
+    *printable_count = 0;
+
+    // when socket is closed by the server, read() returns 0 (EOF)
+    while ((bytes_received = read(sockfd, buffer, sizeof(buffer))) > 0) {
+        for (ssize_t i = 0; i < bytes_received; i++) {
+            if (buffer[i] >= 32 && buffer[i] <= 126) {
+                (*printable_count)++;
+            }
+        }
+    }
+
+    if (bytes_received < 0) {
+        perror("Error reading from socket\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+
+
+int main(int argc, char *argv[]) {
     
     int ret;
     int fd;
     char *file_path;
+    unsigned int printable_count = 0;
 
     // server address structure - ip+port
     struct sockaddr_in server_addr;
@@ -92,18 +155,38 @@ void main(int argc, char *argv[]) {
     }
 
     // create a TCP connection to a specified server - checking for errors
-    int sockfd;
     sockfd = TCP_connect(&server_addr);
     if (sockfd < 0) {
-        close(fd);
         exit(1);
     }
-// transmit data over the TCP connection - checking for errors
 
-// receive data from the TCP connection - checking for errors
+    // transmit data over the TCP connection - checking for errors
+    ret = transmit_data(sockfd, fd);
+    if (ret < 0) {
+        close(fd);
+        close(sockfd);
+        exit(1);
+    }
 
-// print received data to standard output with printf "# of printable characters: %u\n"
+    close(fd); // close the file descriptor after transmission is complete
 
-// exit with 0
+    // indicate the end of data transmission
+    shutdown(sockfd, SHUT_WR);
+
+    // receive data from the TCP connection - checking for errors
+    ret = receive_data(sockfd, &printable_count);
+    if (ret < 0) {
+        close(fd);
+        close(sockfd);
+        exit(1);
+    }   
+
+    // print received data to standard output with printf "# of printable characters: %u\n"
+    printf("# of printable characters: %u\n", printable_count);
+
+    // close the socket
+    close(sockfd);
+    exit(0);
+
 }
 
